1f225073af49f951604995f1eb9ffc66288f60d1
Pons <lanzosuarez@gmail.com>
2017-03-11 21:57:11 +0800

    add jquery

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import routes from './config/routes';
 import '../node_modules/toastr/build/toastr.min.css';
+import '../node_modules/jquery/dist/jquery.min.js';
 
 ReactDom.render(
     routes,
